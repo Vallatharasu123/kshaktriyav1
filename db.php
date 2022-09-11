2a43d11db175c97a7120778af2e6b95b38bca1d4
vallavan raj <vallavanraj150502@gmail.com>
2022-09-11 23:38:12 +0530

    v1

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+$host="localhost";
+$password="";
+$user="root";
+$db_name="kshaktriya";
+$con= mysqli_connect ($host, $user, $password,$db_name) or die('Not connected : Ah sh*t ' . mysqli_connect_error());
+
+?>
---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kshaktriya</title>
+    <link rel="stylesheet" href="font.css">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="button.css">
+</head>
+<body>
+    <section class="topbar">
+    <div class="topbar-content-container">
+    <div class="logo-container">
+                 <img src="assets/img/bdu.png" alt="">
+            </div>
+            <div class="title-text">
+                <h1>School of Computer Science , Engineering & Application</h1>
+                <h2>Bharathidasan University , Tiruchirappalli - 622 023 , Tamilnadu , India.</h2>
+            </div>
+           
+             </div>       
+    </section>
+    <section class="main-container">
+    
+        <div class="kshaktriya">
+        <div class="kshaktriya-topbar">
+        <div class="logo-container">
+                 <img src="assets/img/logo.png" alt="">
+        </div>
+            <div class="titles">
+           <h1>Kshaktriya - 2K22</h1>
+           <h3>Sharpen Your Sword | National Technical Symposium
+     
+           </h3>
+           </div>
+           
+           </div>
+  
+           <div class="container register-button">
+                <div class="btn "><a href="registration.php">register</a></div>
+                
+                <!-- <div class="btn"><a href="#" >Read more 2</a></div>
+				<div class="btn"><a href="#" >Read more 3</a></div> -->
+            </div>
+        </div>
+    </section>
+</body>
+</html>
